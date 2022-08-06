4f676af6e385aae9e0fdbee944105a7a8c2c9231
Pasha Demichev <pasha.demichev@gmail.com>
2022-08-06 23:10:49 +1200

    cleaning files

--- function.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-generate_password() {
-    # Not set at all, give the user a random pass
-    local generated_pwd=$(tr -dc '_A-Za-z0-9!"#$%&<=>?@[\]^' < /dev/urandom | head -c 14)
-    #TODO remove generation promt from the function
-    echo "qBittorent Web UI was generated: $generated_pwd"
-    return $generated_pwd
-}
